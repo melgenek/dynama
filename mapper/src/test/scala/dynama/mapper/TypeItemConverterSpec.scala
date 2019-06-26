8f997ed6967223b698ee33374969293a3e77c98c
Yevhenii Melnyk <ymelnyk@liveintent.com>
2019-06-26 22:15:10 +0200

    Primitive type tests

--- mapper/src/test/scala/dynama/mapper/TypeItemConverterSpec.scala
+++ mapper/src/test/scala/dynama/mapper/TypeItemConverterSpec.scala
@@ -19,6 +19,12 @@ class TypeItemConverterSpec extends FlatSpec with Matchers {
   val intConverter: ItemConverter[IntClass] = ItemConverter.converter[IntClass]
   val doubleConverter: ItemConverter[DoubleClass] = ItemConverter.converter[DoubleClass]
   val floatConverter: ItemConverter[FloatClass] = ItemConverter.converter[FloatClass]
+  val longConverter: ItemConverter[LongClass] = ItemConverter.converter[LongClass]
+  val shortConverter: ItemConverter[ShortClass] = ItemConverter.converter[ShortClass]
+  val byteConverter: ItemConverter[ByteClass] = ItemConverter.converter[ByteClass]
+  val charConverter: ItemConverter[CharClass] = ItemConverter.converter[CharClass]
+  val stringConverter: ItemConverter[StringClass] = ItemConverter.converter[StringClass]
+  val booleanConverter: ItemConverter[BooleanClass] = ItemConverter.converter[BooleanClass]
 
   "ItemConverter" should "convert int class to map" in {
     val test = IntClass(Int.MinValue)
@@ -99,5 +105,120 @@ class TypeItemConverterSpec extends FlatSpec with Matchers {
     result should be(FloatClass(Float.MinValue))
   }
 
+  it should "convert short class to map" in {
+    val test = ShortClass(Short.MinValue)
+
+    val result = shortConverter.toMap(test)
+
+    result should be(Map(
+      "value" -> AttributeValue.builder().n("-32768").build()
+    ))
+  }
+
+  it should "read short class from map" in {
+    val map = Map("value" -> AttributeValue.builder().n("-32768").build())
+
+    val result = shortConverter.fromMap(map)
+
+    result should be(ShortClass(Short.MinValue))
+  }
+
+  it should "convert byte class to map" in {
+    val test = ByteClass(Byte.MinValue)
+
+    val result = byteConverter.toMap(test)
+
+    result should be(Map(
+      "value" -> AttributeValue.builder().n("-128").build()
+    ))
+  }
+
+  it should "read byte class from map" in {
+    val map = Map("value" -> AttributeValue.builder().n("-128").build())
+
+    val result = byteConverter.fromMap(map)
+
+    result should be(ByteClass(Byte.MinValue))
+  }
+
+  it should "convert char class to map" in {
+    val test = CharClass('a')
+
+    val result = charConverter.toMap(test)
+
+    result should be(Map(
+      "value" -> AttributeValue.builder().s("a").build()
+    ))
+  }
+
+  it should "read char class from map" in {
+    val map = Map("value" -> AttributeValue.builder().s("ab").build())
+
+    val result = charConverter.fromMap(map)
+
+    result should be(CharClass('a'))
+  }
+
+  it should "convert long class to map" in {
+    val test = LongClass(Long.MinValue)
+
+    val result = longConverter.toMap(test)
+
+    result should be(Map(
+      "value" -> AttributeValue.builder().n("-9223372036854775808").build()
+    ))
+  }
+
+  it should "read long class from map" in {
+    val map = Map("value" -> AttributeValue.builder().n("-9223372036854775808").build())
+
+    val result = longConverter.fromMap(map)
+
+    result should be(LongClass(Long.MinValue))
+  }
+
+  it should "convert boolean class to map" in {
+    val test = BooleanClass(true)
+
+    val result = booleanConverter.toMap(test)
+
+    result should be(Map(
+      "value" -> AttributeValue.builder().bool(true).build()
+    ))
+  }
+
+  it should "read boolean class from map" in {
+    val map = Map("value" -> AttributeValue.builder().bool(true).build())
+
+    val result = booleanConverter.fromMap(map)
+
+    result should be(BooleanClass(true))
+  }
+
+  it should "read false read boolean class from map when boolean value is not set" in {
+    val map = Map("value" -> AttributeValue.builder().build())
+
+    val result = booleanConverter.fromMap(map)
+
+    result should be(BooleanClass(false))
+  }
+
+  it should "convert string class to map" in {
+    val test = StringClass("abc")
+
+    val result = stringConverter.toMap(test)
+
+    result should be(Map(
+      "value" -> AttributeValue.builder().s("abc").build()
+    ))
+  }
+
+  it should "read string class from map" in {
+    val map = Map("value" -> AttributeValue.builder().s("abc").build())
+
+    val result = stringConverter.fromMap(map)
+
+    result should be(StringClass("abc"))
+  }
 
 }
